e5cf6ebcf678c752ccb77df914d2c8a7406bacbf
haresh2349 <hareshcsolanki07@gmail.com>
2022-11-03 12:02:50 +0530

    Updated

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
-const PORT = 4000;
+const PORT = 8080;
 app.get("/", (req, res) => {
   res.json("WELCOME");
 });
